46117cb4674895dee0137b50e1b6fbb4a999a63c
Renato Tecchio <renatotecchio@gmail.com>
2025-07-06 16:50:51 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 #Kubens instalation for Linux
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
-sudo cp -s /opt/kubectx/kubens /usr/local/bin/kubens
+sudo cp /opt/kubectx/kubens /usr/local/bin/kubens
 
 #Kubectx instalation for Linux
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
-sudo cp -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo cp /opt/kubectx/kubectx /usr/local/bin/kubectx
