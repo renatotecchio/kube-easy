f6c158e4bdcfb00883d4b76b4605fec1b6af3adf
Renato Tecchio <renatotecchio@gmail.com>
2025-07-06 17:52:27 +0100

    Update script.sh
    Refectered script

--- script.sh
+++ script.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-#Kubens instalation for Linux
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
-sudo cp /opt/kubectx/kubens /usr/local/bin/kubens
+set -x
 
-#Kubectx instalation for Linux
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
-sudo cp /opt/kubectx/kubectx /usr/local/bin/kubectx
+local_git_folder="/opt/kubectx"
+local_bin_folder="/usr/local/bin"
+
+#Kubens instalation for Linux
+if [ ! -d  ${local_git_folder} ]; then
+    sudo git clone https://github.com/ahmetb/kubectx ${local_git_folder}
+else
+    sudo sh -c cd $local_git_folder
+    git pull
+fi
+
+if [ ! -e "${local_bin_folder}/kubectx" ]; then
+    sudo ln -s "${local_git_folder}/kubectx" "${local_bin_folder}/kubectx"
+fi
+
+if [ ! -e "${local_bin_folder}/kubens" ]; then
+    sudo ln -s "${local_git_folder}/kubens" "${local_bin_folder}/kubens"
+fi
