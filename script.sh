fe715c04c0928a30d4a4373161b59c76e581c72f
Renato Tecchio <renatotecchio@gmail.com>
2025-07-06 16:48:21 +0100

    Update script.sh
    Replaced the symbolic link for cp instead

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #Kubens instalation for Linux
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo ln -s/opt/kubectx/kubens /usr/local/bin/kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
+sudo cp -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 #Kubectx instalation for Linux
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo \
+sudo cp -s /opt/kubectx/kubectx /usr/local/bin/kubectx
