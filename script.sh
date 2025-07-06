d1d9840b2b7fcccc26ee5b56779bda3190509d94
Renato Tecchio <renatotecchio@gmail.com>
2025-07-06 16:36:49 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Kubens instalation for Linux
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo ln -s/opt/kubectx/kubens /usr/local/bin/kubens
+
+#Kubectx instalation for Linux
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectxsudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
